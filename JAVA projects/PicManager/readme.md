d028969ace6a0be7cf5fbcc243183623586d5499
scheninsp <songchen7788@163.com>
2019-01-20 17:05:30 +0800

    Updated 2019.1.20
    Add PicManager

---
+++ JAVA projects/PicManager/readme.md
@@ -0,0 +1,2 @@
+Updated 2019.1.20
+This is an eclipse project.
---
+++ JAVA projects/PicManager/src/decompress/MainActivity.java
@@ -0,0 +1,61 @@
+package decompress;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import decompress.DeCompressUtil;
+
+public class MainActivity {
+
+	private static ArrayList<String> filelist = new ArrayList<String>();
+
+	static void decompressFiles(String filePath){
+		File root = new File(filePath);
+		File[] files = root.listFiles();
+		for(File file:files){
+			if(file.isDirectory()){
+				System.out.println("Enter: "+ file.getAbsolutePath());
+				decompressFiles(file.getAbsolutePath());
+				filelist.add(file.getAbsolutePath());
+			}
+			else{
+				String file_fullpath = file.getAbsolutePath();
+				System.out.println("Processing: "+ file_fullpath);
+				
+				String type = file_fullpath.substring(file_fullpath.lastIndexOf(".")+1); 
+				//process file according to fileType
+			    if(type.equals("zip") || type.equals("rar")){ 
+			    	try{
+			    		DeCompressUtil.deCompress(file.getAbsolutePath(),filePath);
+			    		file.delete();
+			    	}
+			    	catch(Exception e){
+			    		e.printStackTrace();
+			    	}
+			    }
+			    
+			    else if(type.equals("torrent")){
+			    	try{
+			    		file.delete();
+			    	}
+			    	catch(Exception e){
+			    		e.printStackTrace();
+			    	}
+			    }
+			    
+			    else{
+			    	//not '.zip', '.rar', '.torrent'
+			    	//pass;
+			    }
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+	    String filePath = "C:\\Anime\\temp\\doujin_temp";
+	    decompressFiles(filePath);
+	}
+	
+
+
+}
