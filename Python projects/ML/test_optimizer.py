e5ef08fecbd24da3931fbcc418ea7972af29d62f
scheninsp <songchen7788@163.com>
2019-07-05 22:03:29 +0800

    update 2019.7.5
    update test_optimizer.py

--- Python projects/ML/test_optimizer.py
+++ Python projects/ML/test_optimizer.py
@@ -3,6 +3,7 @@ from scipy.optimize import minimize
 import copy
 import cv2
 from math import sqrt
+import time
 
 WIDTH=114
 HEIGHT=86
@@ -12,15 +13,23 @@ DW = 4
 DH = 4
 
 FACE_AREAS = np.zeros((2,4),dtype = np.float32)
-INDX_GRID = np.zeros ( (HEIGHT, WIDTH, 2), dtype = np.float32)  # indicate original index
-U_GRID = np.zeros ( (HEIGHT, WIDTH, 2), dtype = np.float32)  # undistort grid using radial function
-U_GRID_FLATTEN = U_GRID.flatten()
-EIJ_GRID = np.zeros ( (HEIGHT, WIDTH, 2*DH+1, 2*DW+1, 2), dtype = np.float32)
-# eij from point i to point j, (piy, pix, pjy, pjx , 2)
 
 LAMBDA_F = 4
-LAMBDA_B = 0.2
+LAMBDA_B = 0.5
 LAMBDA_R = 0.1
+
+ADD_CONSTRAIN_BOUNDARY_ORI = True
+NPAD = 4
+
+WIDTH_PAD = WIDTH + 2*NPAD
+HEIGHT_PAD = HEIGHT +2*NPAD
+
+INDX_GRID = np.zeros ( (HEIGHT_PAD, WIDTH_PAD, 2), dtype = np.float32)  # indicate original index
+INDX_GRID_FLATTEN = INDX_GRID.flatten()
+U_GRID = np.zeros ( (HEIGHT_PAD, WIDTH_PAD, 2), dtype = np.float32)  # undistort grid using radial function
+U_GRID_FLATTEN = U_GRID.flatten()
+EIJ_GRID = np.zeros ( (HEIGHT_PAD, WIDTH_PAD, 2*DH+1, 2*DW+1, 2), dtype = np.float32)
+# eij from point i to point j, (piy, pix, pjy, pjx , 2)
 
 
 def distort_coords(coords, cx, cy, k1):
@@ -49,8 +58,8 @@ def myfunc(coords):
     for k in range(kFace):
         for i in range(FACE_AREAS[k, 2], FACE_AREAS[k, 3]):
             for j in range (FACE_AREAS[k, 0], FACE_AREAS[k, 1]):
-                face_areas_indx_x[k].append(2 * WIDTH * i + 2 * j)
-                face_areas_indx_y[k].append(2 * WIDTH * i + 2 * j + 1)
+                face_areas_indx_x[k].append(2 * WIDTH_PAD * i + 2 * j)
+                face_areas_indx_y[k].append(2 * WIDTH_PAD * i + 2 * j + 1)
 
     ef = 0
     for k in range(kFace):
@@ -58,41 +67,41 @@ def myfunc(coords):
 
     # term of || vi - vj ||^2
     er = 0
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(HEIGHT_PAD):
+        for j in range(WIDTH_PAD):
             tmpx = 0
             tmpy = 0
             neighbour_area_indx_x = []
             neighbour_area_indx_y = []
-            for i2 in range(max(0, i-DH),min(HEIGHT,i+DH+1)):
-                for j2 in range(max(0, j-DW),min(WIDTH,j+DW+1)):
-                    neighbour_area_indx_x.append(2 * WIDTH * i2 + 2 * j2)
-                    neighbour_area_indx_y.append(2 * WIDTH * i2 + 2 * j2 + 1)
+            for i2 in range(max(0, i-DH),min(HEIGHT_PAD,i+DH+1)):
+                for j2 in range(max(0, j-DW),min(WIDTH_PAD,j+DW+1)):
+                    neighbour_area_indx_x.append(2 * WIDTH_PAD * i2 + 2 * j2)
+                    neighbour_area_indx_y.append(2 * WIDTH_PAD * i2 + 2 * j2 + 1)
 
             lx = len(neighbour_area_indx_x)
             ly = len(neighbour_area_indx_y)
-            tmpx += np.sum((coords[2 * WIDTH * i + 2 * j] * np.ones(lx, dtype=np.float32) -
+            tmpx += np.sum((coords[2 * WIDTH_PAD * i + 2 * j] * np.ones(lx, dtype=np.float32) -
                      coords[neighbour_area_indx_x]) ** 2)
-            tmpy += np.sum((coords[2 * WIDTH * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
+            tmpy += np.sum((coords[2 * WIDTH_PAD * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
                      coords[neighbour_area_indx_y]) ** 2)
 
             er += (tmpx + tmpy)
 
     # term of || (vi - vj) x eij ||^2
     eb = 0
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(HEIGHT_PAD):
+        for j in range(WIDTH_PAD):
             neighbour_area_indx_x = []
             neighbour_area_indx_y = []
-            for i2 in range(max(0, i - DH), min(HEIGHT, i + DH + 1)):
-                for j2 in range(max(0, j - DW), min(WIDTH, j + DW + 1)):
-                    neighbour_area_indx_x.append(2 * WIDTH * i2 + 2 * j2)
-                    neighbour_area_indx_y.append(2 * WIDTH * i2 + 2 * j2 + 1)
+            for i2 in range(max(0, i - DH), min(HEIGHT_PAD, i + DH + 1)):
+                for j2 in range(max(0, j - DW), min(WIDTH_PAD, j + DW + 1)):
+                    neighbour_area_indx_x.append(2 * WIDTH_PAD * i2 + 2 * j2)
+                    neighbour_area_indx_y.append(2 * WIDTH_PAD * i2 + 2 * j2 + 1)
 
             lx = len(neighbour_area_indx_x)
             ly = len(neighbour_area_indx_y)
-            actual_dh = min(HEIGHT, i + DH + 1) - max(0, i - DH)   #for image boundary regions
-            actual_dw = min(WIDTH, j + DW + 1) - max(0, j - DW)
+            actual_dh = min(HEIGHT_PAD, i + DH + 1) - max(0, i - DH)   #for image boundary regions
+            actual_dw = min(WIDTH_PAD, j + DW + 1) - max(0, j - DW)
 
             #reshape EIJ_GRID into same shape with coords[neighbour_area_indx]
             eij_neighbour_flat =  (EIJ_GRID[i,j,0:actual_dh,0:actual_dw,:]).flatten()
@@ -102,9 +111,9 @@ def myfunc(coords):
             #    print("debug!")
 
             # (xi - xj) * y_eij - x_eij * (yi-yj) , cross-product
-            tmp1 = (coords[2 * WIDTH * i + 2 * j] * np.ones(lx, dtype=np.float32) -
+            tmp1 = (coords[2 * WIDTH_PAD * i + 2 * j] * np.ones(lx, dtype=np.float32) -
                             coords[neighbour_area_indx_x]) * eij_neighbour_flat[list(range(1,len_e,2))]
-            tmp2 = (coords[2 * WIDTH * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
+            tmp2 = (coords[2 * WIDTH_PAD * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
                             coords[neighbour_area_indx_y]) * eij_neighbour_flat[list(range(0,len_e,2))]
 
             # print("debug")
@@ -114,6 +123,28 @@ def myfunc(coords):
     energy = LAMBDA_F * ef + LAMBDA_R * er +LAMBDA_B * eb
 
     return energy
+
+def myfunc_constrain(coords):
+    constrain = 0
+    indx_top = np.asarray(list(range(0,2*WIDTH_PAD))) + 2*WIDTH_PAD *0
+    indx_bottom = np.asarray(list(range(0, 2*WIDTH_PAD))) + 2*WIDTH_PAD *(HEIGHT_PAD - 1)
+    tmp1 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + 0 # x
+    tmp2 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + 1 # y
+    tmp1.append(tmp2)
+    indx_left = np.asarray(tmp1)
+    
+    tmp1 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + (2*WIDTH_PAD-2) # x
+    tmp2 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + (2*WIDTH_PAD-1) # y
+    tmp1.append(tmp2)
+    indx_right = np.asarray(tmp1)
+
+    constrain += coords[indx_top] - INDX_GRID_FLATTEN[indx_top]
+    constrain += coords[indx_bottom] - INDX_GRID_FLATTEN[indx_bottom]
+    constrain += coords[indx_left] - INDX_GRID_FLATTEN[indx_left]
+    constrain += coords[indx_right] - INDX_GRID_FLATTEN[indx_right]
+
+    return constrain
+
     
 def myfunc_der(coords):
     # - coords 1*(2*W*H)
@@ -136,8 +167,8 @@ def myfunc_der(coords):
     for k in range(kFace):
         for i in range(FACE_AREAS[k, 2], FACE_AREAS[k, 3]):
             for j in range(FACE_AREAS[k,0], FACE_AREAS[k,1]):
-                face_areas_indx[k].append(2 * WIDTH * i + 2 * j)
-                face_areas_indx[k].append(2 * WIDTH * i + 2 * j + 1)
+                face_areas_indx[k].append(2 * WIDTH_PAD * i + 2 * j)
+                face_areas_indx[k].append(2 * WIDTH_PAD * i + 2 * j + 1)
 
     for k in range(kFace):
         ef_der[face_areas_indx[k]] = ef_der[face_areas_indx[k]] + \
@@ -146,39 +177,39 @@ def myfunc_der(coords):
     #derivative of er
     er_der = np.zeros(2*nvar, dtype=np.float32)
     
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(HEIGHT_PAD):
+        for j in range(WIDTH_PAD):
             
             tmpx=0
             tmpy=0
             neighbour_area_indx_x = []
             neighbour_area_indx_y = []
             
-            for i2 in range(max(0, i-DH), min(HEIGHT, i+DH+1)):
-                for j2 in range(max(0, j-DW), min(WIDTH, j+DW+1)):
-                    neighbour_area_indx_x.append(2*WIDTH*i2+2*j2)
-                    neighbour_area_indx_y.append(2*WIDTH*i2 + 2*j2 + 1)
+            for i2 in range(max(0, i-DH), min(HEIGHT_PAD, i+DH+1)):
+                for j2 in range(max(0, j-DW), min(WIDTH_PAD, j+DW+1)):
+                    neighbour_area_indx_x.append(2*WIDTH_PAD*i2+2*j2)
+                    neighbour_area_indx_y.append(2*WIDTH_PAD*i2 + 2*j2 + 1)
             
             lx = len(neighbour_area_indx_x)
             ly = len(neighbour_area_indx_y)
-            er_der[2*WIDTH*i + 2*j] = 4 * np.sum(coords[2*WIDTH*i + 2*j] * np.ones(lx, dtype=np.float32) - coords[neighbour_area_indx_x])
-            er_der[2*WIDTH*i + 2*j + 1] = 4 * np.sum(coords[2*WIDTH*i + 2*j + 1] * np.ones(lx, dtype=np.float32) - coords[neighbour_area_indx_y])
+            er_der[2*WIDTH_PAD*i + 2*j] = 4 * np.sum(coords[2*WIDTH_PAD*i + 2*j] * np.ones(lx, dtype=np.float32) - coords[neighbour_area_indx_x])
+            er_der[2*WIDTH_PAD*i + 2*j + 1] = 4 * np.sum(coords[2*WIDTH_PAD*i + 2*j + 1] * np.ones(lx, dtype=np.float32) - coords[neighbour_area_indx_y])
 
     # derivative of eb
     eb_der = np.zeros(2*nvar, dtype=np.float32)
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(HEIGHT_PAD):
+        for j in range(WIDTH_PAD):
             neighbour_area_indx_x = []
             neighbour_area_indx_y = []
-            for i2 in range(max(0, i - DH), min(HEIGHT, i + DH + 1)):
-                for j2 in range(max(0, j - DW), min(WIDTH, j + DW + 1)):
-                    neighbour_area_indx_x.append(2 * WIDTH * i2 + 2 * j2)
-                    neighbour_area_indx_y.append(2 * WIDTH * i2 + 2 * j2 + 1)
+            for i2 in range(max(0, i - DH), min(HEIGHT_PAD, i + DH + 1)):
+                for j2 in range(max(0, j - DW), min(WIDTH_PAD, j + DW + 1)):
+                    neighbour_area_indx_x.append(2 * WIDTH_PAD * i2 + 2 * j2)
+                    neighbour_area_indx_y.append(2 * WIDTH_PAD * i2 + 2 * j2 + 1)
 
             lx = len(neighbour_area_indx_x)
             ly = len(neighbour_area_indx_y)
-            actual_dh = min(HEIGHT, i + DH + 1) - max(0, i - DH)   #for image boundary regions
-            actual_dw = min(WIDTH, j + DW + 1) - max(0, j - DW)
+            actual_dh = min(HEIGHT_PAD, i + DH + 1) - max(0, i - DH)   #for image boundary regions
+            actual_dw = min(WIDTH_PAD, j + DW + 1) - max(0, j - DW)
 
             #reshape EIJ_GRID into same shape with coords[neighbour_area_indx]
             eij_neighbour_flat =  (EIJ_GRID[i,j,0:actual_dh,0:actual_dw,:]).flatten()
@@ -188,15 +219,31 @@ def myfunc_der(coords):
             #    print("debug!")
 
             # (xi - xj) * y_eij - x_eij * (yi-yj) , cross-product
-            tmp1 = (coords[2 * WIDTH * i + 2 * j] * np.ones(lx, dtype=np.float32) -
+            tmp1 = (coords[2 * WIDTH_PAD * i + 2 * j] * np.ones(lx, dtype=np.float32) -
                             coords[neighbour_area_indx_x]) * eij_neighbour_flat[list(range(1,len_e,2))]
-            tmp2 = (coords[2 * WIDTH * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
+            tmp2 = (coords[2 * WIDTH_PAD * i + 2 * j + 1] * np.ones(ly, dtype=np.float32) -
                             coords[neighbour_area_indx_y]) * eij_neighbour_flat[list(range(0,len_e,2))]
 
-            eb_der[2*WIDTH*i + 2*j] = 4 * np.sum((tmp1 - tmp2) * eij_neighbour_flat[list(range(1,len_e,2))])
-            eb_der[2*WIDTH*i + 2*j + 1] = 4 * np.sum((tmp1 - tmp2) *  (-eij_neighbour_flat[list(range(0, len_e, 2))]) )
+            eb_der[2*WIDTH_PAD*i + 2*j] = 4 * np.sum((tmp1 - tmp2) * eij_neighbour_flat[list(range(1,len_e,2))])
+            eb_der[2*WIDTH_PAD*i + 2*j + 1] = 4 * np.sum((tmp1 - tmp2) *  (-eij_neighbour_flat[list(range(0, len_e, 2))]) )
 
     e_der = LAMBDA_F * ef_der + LAMBDA_R * er_der + LAMBDA_B * eb_der
+    
+    if ADD_CONSTRAIN_BOUNDARY_ORI:
+        indx_top = np.asarray(list(range(0,2*WIDTH_PAD))) + 2*WIDTH_PAD *0
+        indx_bottom = np.asarray(list(range(0, 2*WIDTH_PAD))) + 2*WIDTH_PAD *(HEIGHT_PAD - 1)
+        tmp1 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + 0 # x
+        tmp2 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + 1 # y
+        indx_left = np.concatenate((tmp1,tmp2),axis=0)
+
+        tmp1 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + (2*WIDTH_PAD-2) # x
+        tmp2 =  np.asarray(list(range(0, 2*WIDTH_PAD*HEIGHT_PAD, 2*WIDTH_PAD))) + (2*WIDTH_PAD-1) # y
+        indx_right = np.concatenate((tmp1,tmp2),axis=0)
+
+        e_der[indx_left] = 0
+        e_der[indx_right] = 0
+        e_der[indx_top] = 0
+        e_der[indx_bottom] = 0
     
     return e_der
 
@@ -216,51 +263,50 @@ def visualize_grid(coords_grid, output_filename):
 
     #prepare draw board
     pixel_inter = 20
-    new_height = height*pixel_inter
-    new_width = width*pixel_inter
+    new_height = (height+1) * pixel_inter
+    new_width = (width+1) * pixel_inter
     img_draw = 255 * np.ones((new_height, new_width, 3),np.uint8)
 
     #expand meshgrid
     coords_grid = coords_grid * pixel_inter
-    coords_grid_mask = np.all ((coords_grid > [0,0]) & (coords_grid < [new_width, new_height]), axis=-1)
+    coords_grid_mask = np.all ((coords_grid >= [0,0]) & (coords_grid < [new_width, new_height]), axis=-1)
 
     color = (255,0,0)
     thick = 2
     for i in range(height):
         for j in range(width-1):
             if coords_grid_mask[i,j] and coords_grid_mask[i,j+1]:
-                cv2.line(img_draw, tuple(coords_grid[i,j,:]),tuple(coords_grid[i,j+1,:]),
+                cv2.line(img_draw, tuple(coords_grid[i,j,:]+pixel_inter),tuple(coords_grid[i,j+1,:]+pixel_inter),
                      color, thick)
 
     for j in range(width):
         for i in range(height-1):
             if coords_grid_mask[i,j] and coords_grid_mask[i+1,j]:
-                cv2.line(img_draw, tuple(coords_grid[i,j,:]),tuple(coords_grid[i+1,j,:]),
+                cv2.line(img_draw, tuple(coords_grid[i,j,:]+pixel_inter),tuple(coords_grid[i+1,j,:]+pixel_inter),
                      color, thick)
 
     cv2.imwrite(output_filename,img_draw)
 
 if __name__ == "__main__":
 
-    width = WIDTH
-    height = HEIGHT
-
-    cx = width/2
-    cy = height/2
+    cx = WIDTH/2
+    cy = HEIGHT/2
     k_scale = 1 / (cx**2 + cy**2)
     k1 = k_scale * 1.4
 
     face_areas = np.asarray([[10,30,10,40], [80,100,20,50]])
 
-    x,y = np.meshgrid(np.linspace(0, width-1, width), np.linspace(0, height-1, height))
+    x,y = np.meshgrid(np.linspace(0, WIDTH_PAD-1, WIDTH_PAD), np.linspace(0, HEIGHT_PAD-1, HEIGHT_PAD))
     coords = np.stack((x,y), axis=-1)
     INDX_GRID = coords
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    INDX_GRID_FLATTEN = coords.flatten()
+
+    for i in range(HEIGHT_PAD):
+        for j in range(WIDTH_PAD):
             ishift = 0
-            for i2 in range(max(0, i - DH), min(HEIGHT, i + DH + 1)):
+            for i2 in range(max(0, i - DH), min(HEIGHT_PAD, i + DH + 1)):
                 jshift = 0
-                for j2 in range(max(0, j - DW), min(WIDTH, j + DW + 1)):
+                for j2 in range(max(0, j - DW), min(WIDTH_PAD, j + DW + 1)):
                     # ishift = i2 - i + DH
                     # jshift = j2 - j + DW   # do not use this for consistency within myfunc() eb
                     # do not use this to ensure for boundary regions, indices are in the up-left corner in E[i,j,:]
@@ -271,7 +317,7 @@ if __name__ == "__main__":
                     jshift += 1
                 ishift += 1
 
-    u_grid = distort_coords(coords, cx, cy, k1)
+    u_grid = distort_coords(coords, cx+NPAD, cy+NPAD, k1)
 
     coords_flat = coords.flatten()  # [0,0,1,0,...,W,0, ... 0,H,1,H,W,H]
     u_grid_flat = u_grid.flatten()
@@ -290,7 +336,7 @@ if __name__ == "__main__":
     for iIter in range(nIter):
         result1_energy = myfunc(coords_mod)
         result1_der = myfunc_der(coords_mod)
-        # result_der_debug = map_flat_to_grid(result1_der, height, width)  # debug
+        # result_der_debug = map_flat_to_grid(result1_der, HEIGHT_PAD, WIDTH_PAD)  # debug
 
         # alpha_rate = alpha_rate_start / (1+ 0.001 * nIter)
         alpha_rate = alpha_rate_start
@@ -299,8 +345,11 @@ if __name__ == "__main__":
         
         #plot current meshgrid
         if (iIter % 5 == 0):
-            result_grid = map_flat_to_grid(coords_mod, height, width)
+            result_grid = map_flat_to_grid(coords_mod, HEIGHT_PAD, WIDTH_PAD)
             output_filename = "./results/grid_%d.png" % iIter
+
+            # result_grid_crop = result_grid[NPAD:NPAD+HEIGHT-1,NPAD:NPAD+WIDTH-1,:]
+            # visualize_grid(result_grid_crop, output_filename)
             visualize_grid(result_grid, output_filename)
             print("At iteration %d" % iIter)
 
